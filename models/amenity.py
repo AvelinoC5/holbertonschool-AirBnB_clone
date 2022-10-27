020ab5e8ebbbe714a4eb467adcc12b0a3264738d
davesantiarlom <david.arias02@usc.edu.co>
2022-10-26 19:21:45 -0500

    Creation of the 6 models

---
+++ models/amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Amenity Module
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class inherits from BaseModel
+    
+    Attribute:
+        name (str): Public class attribute for Amenity's name
+    """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ initializes parent class """
+        if args and type(args[0]) is dict:
+            BaseModel.__init__(self, args[0])
+        else:
+            BaseModel.__init__(self)
---
+++ models/city.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+City Module
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ City class inherits from BaseModel
+    
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ initializes parent class """
+        if args and type(args[0]) is dict:
+            BaseModel.__init__(self, args[0])
+        else:
+            BaseModel.__init__(self)
