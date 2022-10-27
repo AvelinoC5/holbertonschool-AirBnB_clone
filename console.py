7650d63a8876ed8ab89b98f1b153388a77d3d82e
AvelinoC5 <avelino_63@hotmail.com>
2022-10-26 22:35:02 -0500

    update console.py

--- console.py
+++ console.py
@@ -44,3 +44,92 @@ class HBNBCommand(cmd.Cmd):
             my_inst.save()
             print(my_inst.id)
 
+    def do_show(self, line):
+        """Prints the string representation of
+        an instance based on the class name and id"""
+        # First, separate in a list the commands by white space
+        n = line.split()
+        if not line:
+            print("** class name missing **")
+            return None
+        elif (n[0] not in self.level):
+            print("** class doesn't exist **")
+            return None
+        elif len(n) == 1:
+            print("** instance id missing **")
+            return None
+        else:
+            # concatenate class_name and id with a dot
+            key = "{}.{}".format(n[0], n[1])
+            # search in file json the key saved in key
+            if key not in storage.all().keys():
+                print("** no instance found **")
+            else:
+                obj = storage.all()
+                # string representation of the instance with class name and id
+                print(obj[key])
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        n = line.split()
+        if not line:
+            print("** class name missing **")
+            return None
+        elif (n[0] not in self.level):
+            print("** class doesn't exist **")
+            return None
+        elif len(n) == 1:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(n[0], n[1])
+            if key not in storage.all().keys():
+                print("** no instance found **")
+            else:
+                del storage.all()[key]
+                storage.save()
+
+    def do_all(self, line):
+        """Prints all string representation of
+        all instances based or not on the class name"""
+        n = line.split()
+        obj_list = []
+        if len(n) == 0:
+            for value in storage.all().values():
+                obj_list.append(value.__str__())
+            print(obj_list)
+        elif (n[0] not in self.level):
+            print("** class doesn't exist **")
+        else:
+            for key, value in storage.all().items():
+                if n[0] in key:
+                    obj_list.append(storage.all()[key].__str__())
+                else:
+                    return
+            print(obj_list)
+
+    def do_update(self, line):
+        """Updates an instance based on the class name and id
+        by adding or updating attribute (save the change into the JSON file)
+        Usage: update <class name> <id> <attribute name> "<attribute value>"""
+        n = line.split()
+        if len(n) == 0:
+            print("** class name missing **")
+        elif (n[0] not in self.level):
+            print("** class doesn't exist **")
+        elif len(n) == 1:
+            print("** instance id missing **")
+        else:
+            obj = storage.all()
+            key = "{}.{}".format(n[0], n[1])
+            if (key not in obj):
+                print("** no instance found **")
+            elif len(n) == 2:
+                print("** attribute name missing **")
+            elif len(n) == 3:
+                print("** value missing **")
+            else:
+                # cast to the attribute type
+                # arg_type = type(eval(n[3]))
+                # attr = n[3].strip('\'\"')
+                setattr(obj[key], n[2], n[3])
+                storage.save()
